eea00798ce4536e1b7ef3f9cf97c6ce24a3cd89a
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-03 09:28:24 -0600

    Updated Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import MainHeader from "@/components/main-header";
 
 
 export const metadata: Metadata = {
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <MainHeader />
         {children}
       </body>
     </html>
