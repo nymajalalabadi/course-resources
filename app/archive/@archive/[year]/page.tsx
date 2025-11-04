6f7b985e6044c35086febe05625f311e42450f31
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-04 08:48:06 -0600

    Added news.js

---
+++ app/archive/@archive/[year]/page.tsx
@@ -0,0 +1,7 @@
+export default function ArchiveYearPage() {
+  return (
+    <div>
+      <h1>Archive Year Page</h1>
+    </div>
+  );
+}
--- app/archive/@archive/page.tsx
+++ app/archive/@archive/page.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
 
 export default function ArchivePage() {
   return (
-    <div>
-      <h1>Archive Page</h1>
-    </div>
+    <header id="archive-header">
+      <nav id="archive-filter">
+        <ul>
+            <li>
+                <Link href="/archive/2025">2025</Link>
+            </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
