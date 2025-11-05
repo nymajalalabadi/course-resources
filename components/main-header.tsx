65a5cd0888989ecf6dbea84246309287780d1337
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-05 08:16:49 -0600

    Added usePathname

--- components/main-header.tsx
+++ components/main-header.tsx
@@ -1,6 +1,12 @@
+'use client';
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function MainHeader() {
+
+  const pathname = usePathname();
+  const isActive = (path: string) => pathname.startsWith(path);
+
   return (
     <header id="main-header">
         <h1 id="logo">
@@ -8,10 +14,13 @@ export default function MainHeader() {
         </h1>
       <ul>
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/" className={isActive('/') ? 'active' : ''}>Home</Link>
         </li>
         <li>
-          <Link href="/news">News</Link>
+          <Link href="/news" className={isActive('/news') ? 'active' : ''}>News</Link>
+        </li>
+        <li>
+          <Link href="/archive" className={isActive('/archive') ? 'active' : ''}>Archive</Link>
         </li>
       </ul>
     </header>
