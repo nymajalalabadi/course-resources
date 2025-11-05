86e900009e14033a78d927fa66da7741b71a9bf4
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-05 08:55:49 -0600

    Added InterceptedImagePage

---
+++ app/news/[slug]/(.)image/page.tsx
@@ -0,0 +1,22 @@
+import { notFound } from 'next/navigation';
+import { DUMMY_NEWS } from '@/dummy-news';
+
+export default async function InterceptedImagePage({ params }) {
+    const param = await params;
+    const newsSlug = param.slug;
+
+    const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
+
+    if (!newsItem) {
+        notFound();
+    }
+
+    return (
+        <>
+        <h2>Intercepted Image Page</h2>
+        <div className="fullscreen-image">
+            <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+            </div>
+        </>
+    );
+}
