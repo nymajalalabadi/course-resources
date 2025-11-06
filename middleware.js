e9d12655a86c02d8de20b55a1a3b375765284524
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-06 09:20:00 -0600

    Added config

--- middleware.js
+++ middleware.js
@@ -3,3 +3,11 @@ import { NextResponse } from "next/server";
 export default function middleware(request) {
     return NextResponse.next();
 }
+
+export const config = {
+    matcher: [
+        '/',
+        '/news',
+        '/archive',
+    ],
+}
