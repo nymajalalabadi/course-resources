dde76e5b8f88d437af14522502ae3c6e2a748714
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-07 09:08:59 -0600

    fetching data from backend

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,12 +1,51 @@
-import Link from 'next/link';
-import { DUMMY_NEWS } from '@/dummy-news';
+'use client';
+
 import NewsList from '@/components/news-list';
+import { useEffect, useState } from 'react';
 
 export default function NewsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>();
+  const [news, setNews] = useState();
+
+  useEffect( () => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const response = await fetch('http://localhost:8080/news');
+
+      if (!response.ok) {
+        setError('Failed to fetch news.');
+        setIsLoading(false);
+      }
+
+      const data = await response.json();
+      setNews(data);
+      setIsLoading(false);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+
+  if (news) {
+    newsContent = <NewsList newsItems={news} />;
+  } else {
+    newsContent = <p>No news found.</p>;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList newsItems={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
